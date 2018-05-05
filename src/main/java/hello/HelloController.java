04b6955cfdca234fc09324f96c384f0403be5361
francesco.bisaccia <francesco.bisaccia@accenture.com>
2018-05-05 16:15:14 +0200

    Redis

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,7 +1,11 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 @RestController
 public class HelloController {
@@ -9,7 +13,19 @@ public class HelloController {
 	@RequestMapping("/")
 	public String index() {
 		System.out.println("Hello Francesco, greetings from Spring Boot !");
-		return "Hello Francesco, greetings from Spring Boot !";
+		JedisPool pool = new JedisPool(new JedisPoolConfig(), "redis", 6379, 10, "CpHe6TPt3ch1i8Gm");
+
+		Jedis jedis = pool.getResource();
+		long startTime = System.currentTimeMillis();
+
+		jedis.set("foo", "bar");
+		String foobar = jedis.get("foo");
+		System.out.println("Execution time: " + (System.currentTimeMillis() - startTime));
+
+		pool.returnResource(jedis);
+		pool.close();
+		return ("foo: " + foobar);
+
 	}
 
 }
